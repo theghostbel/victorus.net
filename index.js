1ebbf1ddf34c762d18ea38e8a1a9b30fb11ec96c
Victor Rusakovich <nemiga@gmail.com>
2017-11-22 01:21:28 +0000

    Add server startup script

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿const subdomain =  require('express-subdomain')
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res) {
+  res.send('This is Root homepage')
+});
+
+const subdomainRoutes = express.Router()
+
+subdomainRoutes.get('/', function(req, res) {
+  res.send('This is "BMW App" subdomain')
+});
+
+app.use(subdomain('bmw', subdomainRoutes))
+app.listen(process.env.PORT || 5000)
